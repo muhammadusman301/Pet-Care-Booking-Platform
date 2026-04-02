ba2d325f21a6af645e13aa67d04b8900780a8ba6
muhammadusman301 <40736151@live.napier.ac.uk>
2026-04-02 18:37:17 +0500

    Configured core Django settings with CORS and REST Framework support

--- pet_care_server/core/settings.py
+++ pet_care_server/core/settings.py
@@ -1,1 +1,36 @@
-#settings
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-pet-care-platform-key-2026'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework', # For API development
+    'corsheaders',    # To allow React to talk to Django
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    # ... other standard middleware
+]
+
+CORS_ALLOW_ALL_ORIGINS = True # Professional dev setting
+ROOT_URLCONF = 'core.urls'
+WSGI_APPLICATION = 'core.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
